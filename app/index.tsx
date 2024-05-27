9cc8c47e67b71a87cd61d2814a9b15800857d736
petcomputacaouece <pet.computacao@uece.br>
2024-05-27 19:57:43 -0300

    Projeto reformulado ao padrao inicial

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Image, StyleSheet, Platform, View,Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text style={styles.titleContainer}> Petiano so sofre</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    fontSize:30,
+  },
+  
+});
