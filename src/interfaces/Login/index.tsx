977d247a3638f48ffd9f2235e5672d7d2e0ea45e
Esterfane Camelo <esterfanecamelo@gmail.com>
2024-05-29 22:04:12 -0300

    Tela de Login criada e estilizada

--- src/interfaces/Login/index.tsx
+++ src/interfaces/Login/index.tsx
@@ -1,15 +1,63 @@
-import {Container} from './styles';
-import { Button, Text } from 'react-native';
+import {Container, TitleMarca, ContainerLogin, MessagemLogin, ButtonVoltar, TextButtonVoltar,
+TouchableOpacityEntrar, TextBotaoEntrar, TextInputEmail,TextInputSenha, TouchableOpacityEsqueciASenha, 
+TextBotaoEsqueciASenha, ContainerCriarConta, TouchableOpacityCriarConta, TextCriarConta, TextBotaoCriarConta} from './styles';
+import { Button, Text, TouchableOpacity, TextInput } from 'react-native';
+
 
 export function Login(props){
+
     function telaInicial(){
         props.navigation.navigate('inicial');
-
     }
     return(
         <Container>
-            <Text>aaaaaaaaaa</Text>
-            <Button title='Voltar' onPress={telaInicial}></Button>
+            <TitleMarca>UECE Food</TitleMarca>
+
+            <ContainerLogin>
+                <MessagemLogin>
+                    Olá, faça login na sua conta 
+                </MessagemLogin>
+
+                <TextInputEmail
+                    placeholder="Email"
+                />
+
+                <TextInputSenha
+                    placeholder="Senha"
+                />
+                
+                <TouchableOpacityEntrar>
+                    <TextBotaoEntrar>
+                        Entrar
+                    </TextBotaoEntrar>
+                </TouchableOpacityEntrar>
+
+                <TouchableOpacityEsqueciASenha>
+                    <TextBotaoEsqueciASenha>
+                        Esqueceu sua senha?
+                    </TextBotaoEsqueciASenha>
+                </TouchableOpacityEsqueciASenha>
+
+                <ContainerCriarConta>
+
+                    <TextCriarConta>
+                        Ainda não tem conta?
+                    </TextCriarConta>
+
+                    <TouchableOpacityCriarConta>
+                        <TextBotaoCriarConta>
+                            Criar Conta!
+                        </TextBotaoCriarConta>
+                    </TouchableOpacityCriarConta>
+                    
+                </ContainerCriarConta>
+
+            </ContainerLogin>
+ 
+            <ButtonVoltar onPress={telaInicial}>
+                <TextButtonVoltar>VOLTAR</TextButtonVoltar>
+            </ButtonVoltar> 
+
         </Container>
     )
 }
