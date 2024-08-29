80b83d6dbfc89b5795ed0ecda1005991e1c8b3c6
Vitorfol <vitorfol2302@gmail.com>
2024-08-29 19:23:32 -0300

    Update server.js

--- scripts/server.js
+++ scripts/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const estabelecimentoRouter = require('./routes/registerEstabelecimento.js'); // Caminho para o arquivo registerEstabelecimento.js
 const funcionarioRouter = require('./routes/registerFuncionario.js'); // Caminho para o arquivo registerFuncionario
-const enderecoRouter = require('./routes/registerEndereco.js'); // Caminho para o arquivo registerendereco.js
 const produtoRouter = require('./routes/registerProduto.js'); // Caminho para o arquivo registerProduto.js
 const vendaRouter = require('./routes/registerVenda.js'); // Caminho para o arquivo registerVenda.js
 const userRouter = require('./routes/register.js'); // Caminho para o arquivo register.js
@@ -19,7 +18,6 @@ app.use(express.json());
 app.use('/api', estabelecimentoRouter);
 app.use('/api', userRouter);
 app.use('/api', funcionarioRouter);
-app.use('/api', enderecoRouter);
 app.use('/api', produtoRouter);
 app.use('/api', vendaRouter);
 app.use('/api', loginRoute)
