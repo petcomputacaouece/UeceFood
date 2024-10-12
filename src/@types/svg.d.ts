a1e595e002743da1b0bfa47e6fd52c4f01d94a5b
João Pedro Moreira de Abreu <149326127+jaumPedr@users.noreply.github.com>
2024-10-12 10:37:09 -0300

    Delete src directory
    Organizando feature branch

--- src/@types/svg.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "*.svg" {
-  import React from 'react';
-  import { SvgProps } from "react-native-svg";
-  const content: React.FC<SvgProps>;  //svg podera ser usado como componente
-  export default content;
-}
