3c6d06269ba4febbecc47cb8272bb655b772aa4e
João Pedro Moreira de Abreu <149326127+jaumPedr@users.noreply.github.com>
2024-11-25 18:29:29 -0300

    Ajeitando os midleware
    Separando execução em midlewares, melhorando respostas de status  de servidor.

--- scripts/routes/inventario.js
+++ scripts/routes/inventario.js
@@ -6,70 +6,104 @@ const ROTE_PATH = '/Inventario';
 const router = express.Router();
 const SECRET = 'secreto';
 
-function verifyJWT(req, res, next){
+
+
+
+async function verifyToken(req, res, next){
     const token = req.header('x-access-token');
     jwt.verify(token, SECRET, function (err, decoded) {
         if(err) {
 
-            return res.status(401).end();
+            return res.status(403).json({ message: 'Usuário invalidado' });
         }
+
         req.userId = decoded.userId;
-        console.log(decoded.userId);
         next();
 
     })
 }
 
-
-//Mandar pro cliente os dados do inventario
-router.get(ROTE_PATH,verifyJWT, async (req, res) => {
+async function getCargoFromDB(req, res, next){
 
     const idFuncionario = req.userId;
-    //res.send( await getProdutosFromDB(idFuncionario));
+    const [cargo] = await db.query('SELECT Cargo FROM funcionario WHERE id = ?', idFuncionario);
 
-    const data = await getProdutosFromDB(idFuncionario);
-    console.log(data);
-    res.send(data);
+    req.userCargo = cargo[0];
 
-    async function getProdutosFromDB(idFuncionario){
+    next();
+}
 
-        const [idEstabelecimento] = await db.query('SELECT idEstabelecimento FROM produto WHERE id = ?', idFuncionario);
-        const [produtos] = await db.query('SELECT * FROM produto WHERE idEstabelecimento = ? AND Ativo = 1', idEstabelecimento[0].idEstabelecimento);
+async function getProdutosFromDB(req, res, next){
 
-        return produtos;
+    const idFuncionario = req.userId;
+
+    try{
+            const [idEstabelecimento] = await db.query('SELECT idEstabelecimento FROM produto WHERE id = ?', idFuncionario);
+            const [produtos] = await db.query('SELECT * FROM produto WHERE idEstabelecimento = ? AND Ativo = 1', idEstabelecimento[0].idEstabelecimento);
+
+    }catch(err){
+
+        return res.status(500).json({ message: 'Erro ao acessar Banco de dados' });
+
     }
+
+
+    if(produtos[0].length=0){
+
+        return res.status(200).json({ message: 'Não existe produtos registrados' });
+
+    }
+
+    req.produtoData = produtos[0];
+
+    next();
+}
+
+
+
+//Mandar pro cliente os dados do inventario
+router.get(ROTE_PATH,verifyToken,getProdutosFromDB, async (req, res) => {
+
+    console.log(req.produtoData);
+    res.send(req.produtoData);
+
 
 });
 
 
-//Editar dados de inventario
-router.post(ROTE_PATH, verifyJWT, async (req, res) => {
+// {Editar/Deletar}  dados de inventario
+router.post(ROTE_PATH,verifyToken, getCargoFromDB, async (req, res) => {
+
+    if(req.userCargo == 'Padrao'){
+        res.status(401).json({message: 'Acesso negado'}).end();
+
+    }
+
 
     //json com  array de dados alterados dos produtos
     //O json deve ter todos os dados do pruduto, até os dados que não vão ser alterados
+
     const newData = req.body;
 
-    console.log(newData);
-
     if(newData.Ativo == 0 ){
-        setProdutoInativo(newData);
+        res.status(200).json({message: 'Produto deletado com sucesso'}).end();
     }
     else {
         updateProduto(newData);
+        res.status(200).json({message: 'Dados do Produto editados com sucesso'}).end();
     }
-
-    res.status(200).end();
-
 
     async function updateProduto(infoProduto){
          db.query('UPDATE produto SET Nome = ?, Custo = ?, Preco = ?, PrecoNaMaquina = ?, Desconto = ?, Quantidade = ?  WHERE id = ?', [infoProduto.Nome, infoProduto.Custo, infoProduto.Preco, infoProduto.PrecoMaquina, infoProduto.Desconto, infoProduto.Quantidade, infoProduto.id]);
 
     }
 
-
     async function setProdutoInativo(infoProduto){
         db.query('UPDATE produto SET Ativo = 0 WHERE id = ?', infoProduto.id);
     }
+
 });
+
+
 
 module.exports = router;
